66ad9106358050c771a79f12811f083a0e84ece4
danfengfirst <1596944007@qq.com>
2015-10-30 14:55:53 +0800

    添加注释

--- app/src/main/java/com/team/rhinoceros/story/MainActivity.java
+++ app/src/main/java/com/team/rhinoceros/story/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //添加
         setContentView(R.layout.activity_main);
     }
 }
